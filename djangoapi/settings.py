62cd8175e1785a862159384cfa9b2115e7808313
Enochthedev <enochjesse884@gmail.com>
2022-10-27 21:46:45 +0100

    heroku deployment added

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s$*mi#vvv449qdb=947c1@ncykc%-*rp9-+-w^n!%%#d=-jcf$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://enochs-django-api1.herokuapp.com','http://127.0.0.1']
 
 
 # Application definition
