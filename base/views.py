03e969f539bc934bdba59d1b9abccc4ef5cdcc87
Enochthedev <enochjesse884@gmail.com>
2022-10-28 21:03:52 +0100

    Cors

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 # Create your views here.
 
+# CORS HEADERS
 # create endpoint to return json data
 def endpoint(request):
     data = {
@@ -10,4 +11,9 @@ def endpoint(request):
         "age": 19,
         "bio": "A developer climbing towards his set out goals. too professional . I'm your friendly everyday tech-bro feel free to reach out .A weeb by heart ."
     }
-    return JsonResponse(data, safe=False) 
+    response = JsonResponse(data)
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
+    return response
